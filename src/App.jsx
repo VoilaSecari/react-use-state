a52d918b0305067175f4e151f14bc3daa03e248a
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-24 23:39:04 +0200

    prova utilizzo stati counter + change color

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+//import useState
+import { useState } from "react";
+
 export default function App() {
-  return <h1>My App 2.0</h1>;
+  //qui iniziamo a dichiarare lo useState
+  // let counterState = useState(0);
+  // let counter = counterState[0];
+  // let setCounter = counterState[1];
+
+  //scritta meglio:
+  let [counter, setCounter] = useState(0);
+
+  //un'ìaltra per colore bottone?
+  let [colorRed, setColorRed] = useState(false);
+
+  // qui invece facciamo la funzione che incrementa di uno il click;
+  const handleButtonClick = () => {
+    setCounter(counter + 1);
+    setColorRed((isRed) => !isRed);
+  };
+
+  return (
+    <div className="container">
+      {/* qui vuol dire che prendo il click e lo traccio, è un event listener insomma */}
+      <button
+        className={`btn ${colorRed ? "red" : ""}`}
+        onClick={handleButtonClick}
+      >
+        Il bottone counter
+      </button>
+      <h2>{counter}</h2>
+    </div>
+  );
 }
